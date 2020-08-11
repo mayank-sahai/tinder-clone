fda26fe207825fc482d14acfe8b4d70c1ddc258c
Mayank Sahai <mayank.sahai@newgen.co.in>
2020-08-11 18:26:55 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build a Tinder Clone App 🚀!</h1>
+      
+    </div>
+  );
+}
+
+export default App;
